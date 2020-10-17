fb38772c74e556928ed8bd5d019d223339d59f28
Sam Khan Madadi <sam.madadi.sm@gmail.com>
2020-10-17 11:33:54 +0430

    Adds new icon + Adds enhancements

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
 'use strict'
 
 let urls = '';
+chrome.extension.isAllowedIncognitoAccess(isAllowedAccess);
 
 document.addEventListener('DOMContentLoaded', function () {
-    var copyBtn = document.getElementById('copyUrls');
+    let copyBtn = document.getElementById('copyUrls');
+    let includeIncognito = document.getElementById('includeIncognito');
     copyBtn.addEventListener('click', function () {
-        loadWindowList();
+        urls = '';
+        loadWindowList(includeIncognito.checked);
     });
 });
 
@@ -17,11 +20,16 @@ function copyTextToClipboard(text) {
     });
 }
 
-function loadWindowList() {
+function loadWindowList(includeIncognito) {
     chrome.windows.getAll({ populate: true }, function (windowList) {
-        for (var i = 0; i < windowList.length; i++) {
-            for (var j = 0; j < windowList[i].tabs.length; j++) {
-                urls += windowList[i].tabs[j].url + '\n';
+        for (let i = 0; i < windowList.length; i++) {
+            for (let j = 0; j < windowList[i].tabs.length; j++) {
+                if (includeIncognito)
+                    urls += windowList[i].tabs[j].url + '\n';
+                else {
+                    !windowList[i].tabs[j].incognito ?
+                        (urls += windowList[i].tabs[j].url + '\n') : "";
+                }
             }
         }
         copyTextToClipboard(urls);
@@ -32,4 +40,9 @@ function appendToLog(logLine) {
     document.getElementById('log')
         .appendChild(document.createElement('p'))
         .innerText = logLine;
+}
+
+function isAllowedAccess(allowed) {
+    !allowed ? document.getElementsByClassName('checkbox')[0].style.visibility = 'hidden' : '';
+    !allowed ? appendToLog('To allow in "Incognito mode", go to "Extensions->Copy Tabs->Details and enable "Allow in private"') : '';
 }
